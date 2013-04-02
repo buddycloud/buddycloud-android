1263a27439a8d64e02515b3baf608486f39585d7
abmargb <abmargb@gmail.com>
2013-04-02 09:36:23 -0400

    Commenting feature.

--- src/com/buddycloud/PostDetailsFragment.java
+++ src/com/buddycloud/PostDetailsFragment.java
@@ -1,17 +1,22 @@
 package com.buddycloud;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.buddycloud.card.CommentCard;
 import com.buddycloud.image.SmartImageView;
+import com.buddycloud.model.ModelCallback;
 import com.buddycloud.model.PostsModel;
 import com.buddycloud.utils.AvatarUtils;
 import com.fima.cardsui.views.CardUI;
@@ -28,7 +33,6 @@ public class PostDetailsFragment extends Fragment {
 		final String channelJid = getArguments().getString(SubscribedChannelsFragment.CHANNEL);
 		
 		JSONObject post = PostsModel.getInstance().getById(getActivity(), postId, channelJid);
-		JSONArray comments = PostsModel.getInstance().commentsFromPost(postId);
 
 		((TextView) view.findViewById(R.id.title)).setText(post.optString("author"));
 		
@@ -41,14 +45,78 @@ public class PostDetailsFragment extends Fragment {
 		SmartImageView avatarView = (SmartImageView) view.findViewById(R.id.bcCommentPic);
 		avatarView.setImageUrl(avatarURL, R.drawable.personal_50px);
 		
+		loadComments(view, postId);
+		
+		View postButton = view.findViewById(R.id.postButton);
+		postButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+				final EditText postContent = (EditText) view.findViewById(R.id.postContentTxt);
+				
+				JSONObject post = createPost(postContent);
+				
+				PostsModel.getInstance().save(getActivity(), post, new ModelCallback<JSONObject>() {
+					@Override
+					public void success(JSONObject response) {
+						Toast.makeText(getActivity().getApplicationContext(), "Post created", Toast.LENGTH_LONG).show();
+						postContent.setText("");
+						fetchPosts(channelJid, postId);
+					}
+					
+					@Override
+					public void error(Throwable throwable) {
+						Toast.makeText(getActivity().getApplicationContext(), throwable.getMessage(), Toast.LENGTH_LONG).show();
+					}
+				}, channelJid);
+				
+			}
+
+			private JSONObject createPost(final EditText postContent) {
+				JSONObject post = new JSONObject();
+				try {
+					post.putOpt("content", postContent.getText().toString());
+					post.putOpt("replyTo", postId);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				return post;
+			}
+		});
+		
+		return view;
+	}
+
+	public void fetchPosts(final String channelJid, final String postId) {
+		CardUI cardsUI = (CardUI) getView().findViewById(R.id.postsStream);
+		cardsUI.clearCards();
+		
+		final View progress = getView().findViewById(R.id.subscribedProgress);
+		progress.setVisibility(View.VISIBLE);
+		
+		PostsModel.getInstance().refresh(getActivity(), new ModelCallback<JSONArray>() {
+			@Override
+			public void success(JSONArray response) {
+				progress.setVisibility(View.GONE);
+				loadComments(getView(), postId);
+			}
+			
+			@Override
+			public void error(Throwable throwable) {
+				System.err.println(throwable);
+				
+			}
+		}, channelJid);
+	}
+	
+	private void loadComments(final View view, final String postId) {
+		JSONArray comments = PostsModel.getInstance().commentsFromPost(postId);
 		for (int i = 0; i < comments.length(); i++) {
 			JSONObject comment = comments.optJSONObject(i);
 			CardUI contentView = (CardUI) view.findViewById(R.id.postsStream);
 			contentView.addCard(toCard(comment));
 			contentView.refresh();
 		}
-		
-		return view;
 	}
 	
 	private CommentCard toCard(JSONObject comment) {
