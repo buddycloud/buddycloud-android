e2f507446934dcff4306714696c5883c7c48e82b
Abmar <abmargb@gmail.com>
2013-07-08 10:20:52 -0300

    Fixes #36. Version code updated.

--- src/com/buddycloud/utils/TimeUtils.java
+++ src/com/buddycloud/utils/TimeUtils.java
@@ -5,12 +5,16 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class TimeUtils {
 	private TimeUtils() {}
 	
 	public static final DateFormat ISO_8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
 	public static final String OLDEST_DATE = "1970-01-01T00:00:00.000Z";
+	static {
+		ISO_8601.setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
 	
 	public static Date fromISOToDate(String isoDate) throws ParseException {
 		return ISO_8601.parse(isoDate);
