9e33e35ec8b777d489343a1d7e7b17b5c7bc1dd0
Abmar <abmargb@gmail.com>
2013-08-18 15:21:48 -0300

    Fixes #52.

--- src/com/buddycloud/utils/TextUtils.java
+++ src/com/buddycloud/utils/TextUtils.java
@@ -7,6 +7,7 @@ public class TextUtils {
 
 	public static Spanned anchor(String text) {
 		text = text.replaceAll("(\\S+://[^<>[:space:]]+[[:alnum:]/])", "<a href=\"$1\">$1</a>");
+		text = text.replaceAll("\\n", "<br>");
 		return Html.fromHtml(text);
 	}
 }
