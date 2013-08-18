cede35e8638e6d33105d321f22aae256b09dcd23
Abmar <abmargb@gmail.com>
2013-08-18 17:01:49 -0300

    Fixes #66.

--- src/com/buddycloud/ChannelDetailActivity.java
+++ src/com/buddycloud/ChannelDetailActivity.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.buddycloud.fragments.GenericChannelsFragment;
 import com.buddycloud.model.ChannelMetadataModel;
+import com.buddycloud.model.ModelCallback;
 import com.buddycloud.model.SubscribedChannelsModel;
 import com.buddycloud.utils.AvatarUtils;
 import com.buddycloud.utils.ImageHelper;
@@ -20,15 +21,43 @@ public class ChannelDetailActivity extends SherlockActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_channel_details);
 		
-		String channelJid = getIntent().getStringExtra(GenericChannelsFragment.CHANNEL);
-		String role = getIntent().getStringExtra(SubscribedChannelsModel.ROLE);
+		final String channelJid = getIntent().getStringExtra(GenericChannelsFragment.CHANNEL);
+		final String role = getIntent().getStringExtra(SubscribedChannelsModel.ROLE);
 		
-		boolean editable = role.equals(SubscribedChannelsModel.ROLE_OWNER)
-				|| role.equals(SubscribedChannelsModel.ROLE_MODERATOR);
+		final boolean editable = role != null && (role.equals(SubscribedChannelsModel.ROLE_OWNER)
+				|| role.equals(SubscribedChannelsModel.ROLE_MODERATOR));
 
 		setTitle(channelJid);
 		
+		if (!fillFields(editable, channelJid)) {
+			ChannelMetadataModel.getInstance().fill(getApplicationContext(), new ModelCallback<Void>() {
+				@Override
+				public void success(Void response) {
+					fillFields(editable, channelJid);
+				}
+				
+				@Override
+				public void error(Throwable throwable) {
+				}
+			}, channelJid);
+		}
+		
+		ImageView avatarView = (ImageView) findViewById(R.id.avatarView);
+		String avatarURL = AvatarUtils.avatarURL(this, channelJid);
+		ImageHelper.picasso(this).load(avatarURL)
+				.placeholder(R.drawable.personal_50px)
+				.error(R.drawable.personal_50px)
+				.into(avatarView);
+	}
+
+	private boolean fillFields(boolean editable, String channelJid) {
+		
 		JSONObject metadata = ChannelMetadataModel.getInstance().getFromCache(this, channelJid);
+		
+		if (metadata == null) {
+			return false;
+		}
+		
 		EditText titleTxt = (EditText) findViewById(R.id.titleTxt);
 		titleTxt.setText(metadata.optString("title"));
 		setEditable(editable, titleTxt);
@@ -43,27 +72,22 @@ public class ChannelDetailActivity extends SherlockActivity {
 		
 		EditText creationDateTxt = (EditText) findViewById(R.id.creationDateTxt);
 		creationDateTxt.setText(metadata.optString("creationDate"));
-		setEditable(false, descriptionTxt);
+		setEditable(false, creationDateTxt);
 		
 		EditText channelTypeTxt = (EditText) findViewById(R.id.channelTypeTxt);
 		channelTypeTxt.setText(metadata.optString("channelType"));
-		setEditable(false, descriptionTxt);
+		setEditable(false, channelTypeTxt);
 		
 		EditText defaultAffiliationTxt = (EditText) findViewById(R.id.defaultAffiliationTxt);
 		defaultAffiliationTxt.setText(metadata.optString("defaultAffiliation"));
-		setEditable(editable, descriptionTxt);
+		setEditable(editable, defaultAffiliationTxt);
 		
-		ImageView avatarView = (ImageView) findViewById(R.id.avatarView);
-		String avatarURL = AvatarUtils.avatarURL(this, channelJid);
-		ImageHelper.picasso(this).load(avatarURL)
-				.placeholder(R.drawable.personal_50px)
-				.error(R.drawable.personal_50px)
-				.into(avatarView);
+		return true;
 	}
 
-	private void setEditable(boolean editable, EditText titleTxt) {
+	private void setEditable(boolean editable, EditText editText) {
 		if (!editable) {
-			titleTxt.setKeyListener(null);
+			editText.setKeyListener(null);
 		}
 	}
 
