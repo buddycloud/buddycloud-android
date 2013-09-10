8a716266f17940a1db97d5fa3b9255cde0fc8b24
Abmar <abmargb@gmail.com>
2013-09-10 10:19:39 -0300

    Fixes #78/

--- src/com/buddycloud/utils/TimeUtils.java
+++ src/com/buddycloud/utils/TimeUtils.java
@@ -1,25 +1,19 @@
 package com.buddycloud.utils;
 
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
 
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 import org.json.JSONObject;
 
 public class TimeUtils {
-	private TimeUtils() {}
 	
-	public static final DateFormat ISO_8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
+	private static final DateTimeFormatter ISO_8601_PARSER = ISODateTimeFormat.dateTimeParser();
 	public static final String OLDEST_DATE = "1970-01-01T00:00:00.000Z";
-	static {
-		ISO_8601.setTimeZone(TimeZone.getTimeZone("UTC"));
-	}
 	
 	public static Date fromISOToDate(String isoDate) throws ParseException {
-		return ISO_8601.parse(isoDate);
+		return ISO_8601_PARSER.parseDateTime(isoDate).toDate();
 	}
 	
 	public static Date updated(JSONObject post) throws ParseException {
