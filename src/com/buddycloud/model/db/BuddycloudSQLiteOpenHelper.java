c9b4fe2eb4b053c9f162472e3964e7359dbc7559
Abmar <abmargb@gmail.com>
2013-09-10 12:05:45 -0300

    Updated version codes.

--- src/com/buddycloud/model/db/BuddycloudSQLiteOpenHelper.java
+++ src/com/buddycloud/model/db/BuddycloudSQLiteOpenHelper.java
@@ -7,7 +7,7 @@ import android.util.Log;
 
 public class BuddycloudSQLiteOpenHelper extends SQLiteOpenHelper {
 	
-	public static final int DATABASE_VERSION = 8;
+	public static final int DATABASE_VERSION = 9;
 	public static final String DATABASE_NAME = "buddycloud.db";
 	
 	public BuddycloudSQLiteOpenHelper(Context context) {
