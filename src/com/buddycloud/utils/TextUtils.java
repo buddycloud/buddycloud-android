a97159227479fcea8700cd333d7cf00d3ebe2ac0
Abmar <abmargb@gmail.com>
2013-12-13 10:56:02 -0400

    TextUtils: Anchoring jids

--- src/com/buddycloud/utils/TextUtils.java
+++ src/com/buddycloud/utils/TextUtils.java
@@ -1,15 +1,30 @@
 package com.buddycloud.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.text.Html;
 import android.text.Spanned;
 
 public class TextUtils {
 
+	private static final String LINKS_REGEX = new StringBuilder()
+			.append("(?:")
+			.append("\\b((?:[a-z][\\w-]+:(?:/{1,3}|[a-z0-9%])|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]"
+					+ "{2,4}/)(?:[^\\s()<>]+|\\((?:[^\\s()<>]+|(?:\\([^\\s()<>]+\\)))*\\))+(?:\\((?:"
+					+ "[^\\s()<>]+|(?:\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:\'\".,<>?«»“”‘’]))\\b")
+			.append("|")
+			.append("\\b([\\w\\d][\\w\\d-_%&+<>.]+@[\\w\\d-]{3,}\\.[\\w\\d-]{2,}(?:\\.[\\w]{2,6})?)\\b")
+			.append(")").toString();
+	
+	private static final Pattern LINKS_PATTERN = Pattern.compile(LINKS_REGEX);
+	
 	public static Spanned anchor(String text) {
 		if (text == null) {
 			return null;
 		}
-		text = text.replaceAll("(\\S+://[^<>[:space:]]+[[:alnum:]/])", "<a href=\"$1\">$1</a>");
+		Matcher matcher = LINKS_PATTERN.matcher(text);
+		text = matcher.replaceAll("<a href=\"$0\">$0</a>");
 		text = text.replaceAll("\\n", "<br>");
 		return Html.fromHtml(text);
 	}
