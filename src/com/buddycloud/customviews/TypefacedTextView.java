ffcb7898d8405b76582b4b477456331211431a4b
Deminem <adnan.urooj@hotmail.com>
2015-02-23 04:30:21 +0500

    Other minor tweaks
    Other minor tweaks

--- src/com/buddycloud/customviews/TypefacedTextView.java
+++ src/com/buddycloud/customviews/TypefacedTextView.java
@@ -2,6 +2,10 @@ package com.buddycloud.customviews;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.support.v4.util.LruCache;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -23,24 +27,49 @@ import com.buddycloud.utils.TypefacesUtil;
  */
 public class TypefacedTextView extends TextView {
 
+	private static LruCache<String, Typeface> sTypefaceCache = new LruCache<String, Typeface>(12);
+
 	private static final String FONTS_PATH = "fonts/";
 	
-    public TypefacedTextView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+	public TypefacedTextView(Context context) {
+		super(context);
+	}
 
-        //Typeface.createFromAsset doesn't work in the layout editor. Skipping...
-        if (isInEditMode()) {
-            return;
-        }
+	public TypefacedTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		applyFonts(context, attrs);
+	}
 
-        TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.Typefaced);
-        String fontName = styledAttrs.getString(R.styleable.Typefaced_typeface);
-        styledAttrs.recycle();
+	public TypefacedTextView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		applyFonts(context, attrs);
+	}
 
-        if (fontName != null) {
-        	// use typefaces cache to resolve memory leak issue
-            setTypeface(TypefacesUtil.get(context, FONTS_PATH + fontName));
-        }
-    }
+	private void applyFonts(Context context, AttributeSet attrs) {
+
+		// Get our custom attributes
+		TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+				R.styleable.Typefaced, 0, 0);
+
+		try {
+			String fontName = a.getString(R.styleable.Typefaced_typeface);
+			if (!isInEditMode() && !TextUtils.isEmpty(fontName)) {
+				Typeface typeface = sTypefaceCache.get(fontName);
+
+				if (typeface == null) {
+					typeface = TypefacesUtil.get(context, FONTS_PATH + fontName);
+
+					// Cache the Typeface object
+					sTypefaceCache.put(fontName, typeface);
+				}
+				setTypeface(typeface);
+
+				// Note: This flag is required for proper typeface rendering
+				setPaintFlags(getPaintFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+			}
+		} finally {
+			a.recycle();
+		}
+	}
 
 }
